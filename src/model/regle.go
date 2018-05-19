ab1bc6cca9f5663409f02dc6a0e4eb61479835a5
florianrambur <flo.rambur@gmail.com>
2018-05-19 19:09:32 +0200

    Mise en place du CRUD pour les Champs

--- src/model/regle.go
+++ src/model/regle.go
@@ -5,3 +5,51 @@ type Regle struct {
 	Nom        string      `json:"nom"`
 	Parametres []Parametre `json:"parametres"`
 }
+
+
+func (regle *Regle) Create() {
+	stmt, err := db.Prepare("INSERT INTO regle (Nom) VALUE (?)")
+	help.CheckErr(err)
+
+	reponse, err := stmt.Exec(regle.Nom)
+	help.CheckErr(err)
+
+	id, err := reponse.LastInsertId()
+	help.CheckErr(err)
+
+	regle.Id = int(id)
+}
+
+func (regle *Regle) Delete() bool {
+	stmt, err := db.Prepare("DELETE FROM regle WHERE id=?")
+	help.CheckErr(err)
+
+	reponse, err := stmt.Exec(champ.Id)
+	help.CheckErr(err)
+
+	affect, err := reponse.RowsAffected()
+	help.CheckErr(err)
+
+	if affect != 1 {
+		return false
+	}
+	return true
+}
+
+func GetRegles(max int) []Regle {
+	var regles []Regle
+
+	stmt, err := db.Query("SELECT * FROM regle LIMIT ?")
+	help.CheckErr(err)
+
+	for stmt.Next() {
+		var id int
+		var nom string
+		err = stmt.Scan(&id, &nom)
+		help.CheckErr(err)
+
+		regles = append(regles, Regle{Id: id, Nom: nom})
+	}
+
+	return regles
+}
