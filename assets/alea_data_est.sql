ffa0b55f71d2e823dc37a7f65476811912ee0469
2alheure <jojo77.juv@live.fr>
2018-05-21 00:38:03 +0200

    Mieux avec la bonne version de la BDD.

--- assets/alea_data_est.sql
+++ assets/alea_data_est.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 20 mai 2018 à 13:08
+-- Généré le :  Dim 20 mai 2018 à 22:36
 -- Version du serveur :  5.7.21
 -- Version de PHP :  5.6.35
 
@@ -37,6 +37,17 @@ CREATE TABLE `champ` (
   `clef` varchar(255) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Déchargement des données de la table `champ`
+--
+
+INSERT INTO `champ` (`id`, `ressource_id`, `clef`) VALUES
+(1, 1, 'nom'),
+(2, 1, 'prenom'),
+(3, 1, 'age'),
+(4, 1, 'sexe'),
+(5, 1, 'ville');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +60,18 @@ CREATE TABLE `champ_parametre` (
   `regle_parametre_id` int(10) UNSIGNED NOT NULL,
   `valeur` varchar(255) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `champ_parametre`
+--
+
+INSERT INTO `champ_parametre` (`id`, `champ_id`, `regle_parametre_id`, `valeur`) VALUES
+(8, 1, 8, 'nom'),
+(9, 2, 8, 'prenom'),
+(10, 3, 10, '100'),
+(11, 4, 1, '[Homme|Femme]'),
+(12, 5, 8, 'ville'),
+(13, 3, 9, '0');
 
 -- --------------------------------------------------------
 
@@ -95,7 +118,9 @@ INSERT INTO `regle` (`id`, `nom`) VALUES
 (6, 'Egal'),
 (7, 'Pair'),
 (8, 'Impair'),
-(9, 'Multiple de');
+(9, 'Multiple de'),
+(10, 'Dictionnaire'),
+(11, 'Compris entre');
 
 -- --------------------------------------------------------
 
@@ -120,7 +145,10 @@ INSERT INTO `regle_parametre` (`id`, `regle_id`, `parametre_id`) VALUES
 (4, 4, 3),
 (5, 5, 3),
 (6, 6, 3),
-(7, 9, 2);
+(7, 9, 2),
+(8, 10, 2),
+(9, 11, 4),
+(10, 11, 4);
 
 -- --------------------------------------------------------
 
@@ -134,6 +162,15 @@ CREATE TABLE `ressource` (
   `createur` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `ressource`
+--
+
+INSERT INTO `ressource` (`id`, `nom`, `createur`, `date_creation`) VALUES
+(1, 'user', '2alheure', '2018-05-20 13:27:38'),
+(2, 'machin', 'frambur', '2018-05-20 15:34:43'),
+(3, 'truc', '2dtension', '2018-05-20 15:34:43');
 
 --
 -- Index pour les tables déchargées
@@ -189,13 +226,13 @@ ALTER TABLE `ressource`
 -- AUTO_INCREMENT pour la table `champ`
 --
 ALTER TABLE `champ`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `champ_parametre`
 --
 ALTER TABLE `champ_parametre`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT pour la table `parametre`
@@ -207,19 +244,19 @@ ALTER TABLE `parametre`
 -- AUTO_INCREMENT pour la table `regle`
 --
 ALTER TABLE `regle`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `regle_parametre`
 --
 ALTER TABLE `regle_parametre`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `ressource`
 --
 ALTER TABLE `ressource`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Contraintes pour les tables déchargées
