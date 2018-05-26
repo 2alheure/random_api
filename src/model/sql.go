3484d1db297100af38b633f83363154389db7e8b
2alheure <jojo77.juv@live.fr>
2018-05-26 14:15:58 +0200

    Route /ressource FONCTIONNELLE !

--- src/model/sql.go
+++ src/model/sql.go
@@ -24,23 +24,27 @@ func InitBdd() {
 // func getRessourceFullStructure(id int) string {
 // 	str := "
 	// SELECT 
-	// 	champ.clef AS clef,
-	// 	champ.id AS champ_id,
-	// 	regle.nom AS regle,
-	// 	regle.id AS regle_id,
+	// champ.clef AS clef,
+	// champ.id AS champ_id,
+	// regle.nom AS regle,
+	// regle.id AS regle_id,
+	// CONCAT(
+	// 	'[',
 	// 	GROUP_CONCAT(
 	// 		CONCAT(
-	// 			'{\"id\": \"', champ_parametre.id, '\", '
+	// 			'{\"id\": ', champ_parametre.id, ', '
 	// 			'\"type\": \"', parametre.nom, '\", '
 	// 			'\"value\": \"', champ_parametre.valeur, '\"}'
 	// 		) ORDER BY regle_parametre.id
-	// 	) AS parametres
+	// 	),
+	// 	']'
+	// ) AS parametres
 	// FROM ressource
-	// 	LEFT OUTER JOIN champ ON champ.ressource_id = ressource.id
-	// 	LEFT OUTER JOIN champ_parametre ON champ_parametre.champ_id = champ.id
-	// 	LEFT OUTER JOIN regle_parametre ON champ_parametre.regle_parametre_id = regle_parametre.id
-	// 	LEFT OUTER JOIN regle ON regle_parametre.regle_id = regle.id
-	// 	LEFT OUTER JOIN parametre ON regle_parametre.parametre_id = parametre.id
+	// LEFT OUTER JOIN champ ON champ.ressource_id = ressource.id
+	// LEFT OUTER JOIN champ_parametre ON champ_parametre.champ_id = champ.id
+	// LEFT OUTER JOIN regle_parametre ON champ_parametre.regle_parametre_id = regle_parametre.id
+	// LEFT OUTER JOIN regle ON regle_parametre.regle_id = regle.id
+	// LEFT OUTER JOIN parametre ON regle_parametre.parametre_id = parametre.id
 	// WHERE ressource.id = 1
 	// GROUP BY champ_parametre.champ_id
 	// ORDER BY clef
