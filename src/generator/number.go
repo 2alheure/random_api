1515bcea0f6bacf44adbc699b033947299cd3b23
florianrambur <flo.rambur@gmail.com>
2018-05-26 22:59:39 +0200

    Generate LowerThan & StrictlyLowerThan number function

--- src/generator/number.go
+++ src/generator/number.go
@@ -2,8 +2,33 @@ package generator;
 
 import (
 	"fmt"
+	rand "math/rand"
 );
 
 func TestNum() {
 	fmt.Println("test num_gen");
 }
+
+func LowerThan(n int) int {
+	var ret int;
+	ret = rand.Intn(n+1);
+
+	return ret;
+}
+
+func StrictlyLowerThan(n int) int {
+	var ret int;
+	ret = rand.Intn(n);
+
+	return ret;
+}
+
+/**
+func greaterThan(n int) int {
+
+}
+
+func strictlyGreaterThan() int {
+
+}
+*/
--- src/test.go
+++ src/test.go
@@ -7,6 +7,8 @@ import (
 func main() {
     fmt.Printf("hello, world\n");
     test := generator.FromRegex("test", 10);
+    fmt.Println(generator.LowerThan(5));
+    fmt.Println(generator.StrictlyLowerThan(5));
 
     fmt.Println(test);
 }
