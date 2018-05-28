c47dc96f83c578b8a4312a1c8b3f9739b5a69199
KCLX\jordanj <jojo77.juv>
2018-05-28 18:40:22 +0200

    avancée LCB parametres

--- src/controller/parametres.go
+++ src/controller/parametres.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"net/http"
 	// "strconv"
-	"fmt"
+	_ "fmt"
 	json "encoding/json"
 
 	help "random_api/src/helper"
@@ -11,19 +11,35 @@ import (
 )
 
 func SetParametres(w http.ResponseWriter, r *http.Request) {
-	// champ_id := r.FormValue("champ_id")
+	champ_id := r.FormValue("champ_id")
 	parametres := r.FormValue("parametres")
 
-	var p []string
-	err := json.Unmarshal([]byte(parametres), &p)
-
-	help.CheckErr(err)
-
-	fmt.Println(p)
+	var params []string
+	err := json.Unmarshal([]byte(parametres), &params)
+	
+	if champ_id == "" || err != nil {
+		Return(w, 400, nil)
+	} else {
+		if model.SetParametres(champ_id, parametres) {
+			ReturnOk(w)
+		} else {
+			Return(w, 404, nil)
+		}
+	}
 }
 
 func ResetParametres(w http.ResponseWriter, r *http.Request) {
-	// champ_id := r.FormValue("champ_id")
+	champ_id := r.FormValue("champ_id")
+
+	if champ_id == "" {
+		Return(w, 400, nil)
+	} else {
+		if model.ResetParametres(champ_id) {
+			ReturnOk(w)
+		} else {
+			Return(w, 404, nil)
+		}
+	}
 }
 
 func OptionsParametres(w http.ResponseWriter, r *http.Request) {
--- src/model/parametre.go
+++ src/model/parametre.go
@@ -5,3 +5,32 @@ type Parametre struct {
 	Type  string `json:"type,omitempty"`
 	Value string `json:"value,omitempty"`
 }
+
+func SetParametres(champ_id int, parametres []string) {
+	for _, val := range parametres {
+		stmt, err := Bdd.Prepare("INSERT INTO")
+		/*
+https://stackoverflow.com/questions/25674737/mysql-update-multiple-rows-with-different-values-in-one-query
+https://stackoverflow.com/questions/3870540/how-to-update-column-with-null-value
+https://stackoverflow.com/questions/2472229/insert-into-select-from-on-duplicate-key-update
+		*/
+		help.CheckErr(err)
+		
+		_, err = stmt.Exec(val[0], ress.Id)
+		help.CheckErr(err)
+	}
+}
+
+func ResetParametres(champ_id int) {
+	stmt, err := Bdd.Prepare("UPDATE champ_parametre SET valeur = NULL WHERE champ_id = ?")
+	help.CheckErr(err)
+	
+	ret, err := stmt.Exec(champ_id)
+	help.CheckErr(err)
+
+	if ret < 1 {
+		return false
+	}
+	return true
+}
+
