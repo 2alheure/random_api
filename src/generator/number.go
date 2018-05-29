305a9a5ea1375c6fe3b40abc9ad7456ed0198932
florianrambur <flo.rambur@gmail.com>
2018-05-29 12:45:21 +0200

    Modification des generator + ajout de equal

--- src/generator/number.go
+++ src/generator/number.go
@@ -3,47 +3,134 @@ package generator;
 import (
 	"fmt"
 	rand "math/rand"
+	constante "math/const"
+	"errors"
+	"strconv"
 	"time"
+
+	model "../src/model"
 );
 
 func TestNum() {
 	fmt.Println("test num_gen");
 }
 
-func BetweenMinAndMax(min int, max int) int {
-	rand.Seed(time.Now().UnixNano());
-	var ret int;
-	ret = rand.Intn(max - min) + min;
+func BetweenMinAndMax(args []string) (ChampKV, error) {
+	var toReturn ChampKV
 
-	return ret;
+	if len(args) == 2 {
+		minStr := args[0]
+		maxStr := args[1]
+
+		min, err := strconv.Atoi(minStr)
+		if err != nil {
+			return toReturn, err
+		}
+
+		max, err := strconv.Atoi(maxStr)
+		if err != nil {
+			return toReturn, err
+		}
+
+		if min > max {
+			return BetweenMinAndMax(maxStr, minStr)
+		}
+
+		rand.Seed(time.Now().UnixNano());
+		var ret int;
+		ret = rand.Intn(max - min) + min;
+
+		return ChampKV{Int: ret}, nil
+	} else {
+		return toReturn, errors.New("Bad argument number.")
+	}
 }
 
-func LowerThan(n int) int {
-	rand.Seed(time.Now().UnixNano());
-	var ret int;
-	ret = rand.Intn(n+1);
+func Equal(args []string) (ChampKV, error) {
+	var toReturn ChampKV
 
-	return ret;
+	if len(args) == 1 {
+		nStr := args[0]
+
+		n, err := strconv.Atoi(nStr)
+		if err != nil {
+			return toReturn, err
+		}
+
+		return ChampKV{Int: ret}, nil
+	} else {
+		return toReturn, errors.New("Bad argument number.")
+	}
 }
 
-func StrictlyLowerThan(n int) int {
-	rand.Seed(time.Now().UnixNano());
-	var ret int;
-	ret = rand.Intn(n);
+func LowerThan(args []string) (ChampKV, error) {
+	var toReturn ChampKV
 
-	return ret;
+	if len(args) == 1 {
+		nStr := args[0]
+		
+		n, err := strconv.Atoi(nStr)
+		if err != nil {
+			return toReturn, err
+		}
+
+		rand.Seed(time.Now().UnixNano())
+		var ret int
+		ret = rand.Intn(n+1)
+
+		return ChampKV{Int: ret}, nil
+	} else {
+		return toReturn, errors.New("Bad argument number.")
+	}
 }
 
-/**
-func GreaterThan(n int) int {
-	rand.Seed(time.Now().UnixNano());
-	var ret int;
-	ret = rand.Int();
+func StrictlyLowerThan(args []string) (ChampKV, error) {
+	var toReturn ChampKV
 
-	return ret;
+	if len(args) == 1 {
+		nStr := args[0]
+
+		n, err := strconv.Atoi(nStr)
+		if err != nil {
+			return toReturn, err
+		}
+
+		rand.Seed(time.Now().UnixNano())
+		var ret int
+		ret = rand.Intn(n)
+
+		return ChampKV{Int: ret}, nil
+	} else {
+		return toReturn, errors.New("Bad argument number.")
+	}
 }
 
-func strictlyGreaterThan() int {
+func EvenNumber(args []string) (ChampKV, error) {
+	var toReturn ChampKV
 
+	if len(args) == 0 {
+		rand.Seed(time.Now().UnixNano())
+		var ret int
+		ret = (rand.Intn(int(constante.MaxInt32))) * 2
+
+		return ChampKV{Int: ret}, nil
+	} else {
+		return toReturn, errors.New("Bad argument number.")
+	}
 }
-*/
+
+func OddNumber(args []string) (ChampKV, error) {
+	var toReturn ChampKV
+
+	if len(args) == 0 {
+		rand.Seed(time.Now().UnixNano())
+		var ret int
+		ret = rand.Intn(int(constante.MaxInt32))
+		ret += (ret % 2) + 1
+
+		return ChampKV{Int: ret}, nil
+	} else {
+		return toReturn, errors.New("Bad argument number.")
+	}
+}
+
